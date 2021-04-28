d39cc314062428387f216f5e359f5f1a6fb2483e
ronaldemanuel <emronald15el@gmail.com>
2021-04-28 11:45:07 -0300

    Listagem de pets

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const path = require("path")
 const mongoose = require("mongoose")
+const handlebars = require("express-handlebars")
 require("./modelos/Pet")
 const Pet = mongoose.model("pets")
 require("./modelos/Usuario")
@@ -17,6 +18,7 @@ const app = express()
         saveUninitialized: true
     }))    
     app.use(flash())
+
     //Midleware
     app.use((req, res, next) => {
         res.locals.sucesso_msg = req.flash("sucesso_msg")
@@ -26,8 +28,14 @@ const app = express()
     // Body Parser
     app.use(express.urlencoded({extended: true}))
     app.use(express.json())
+
+    // Handlebars
+    app.engine('handlebars', handlebars({defaultLayout: "main"}))
+    app.set('view engine', 'handlebars')
+
     // Caminho público
     app.use(express.static(path.join(__dirname, "publico")))
+
     // MongoDB
     mongoose.Promise = global.Promise
     mongoose.connect("mongodb://localhost/pata_aqui", {
@@ -42,20 +50,24 @@ const app = express()
 
 // Rotas
 app.get('/', (req, res) => {
-    res.render("index")
+    Pet.find().lean().then(pets => {
+        res.render("home", {pets: pets})
+    }).catch(err => {
+        req.flash("erro_msg", "Houve um erro ao listar os pets")
+    })
 })
 
 app.get('/sobre', (req, res) => {
-    res.sendFile(__dirname + "/publico/paginas/sobre.html")
+    res.render("pages/sobre")
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname + "/publico/paginas/login.html")
+    res.render("pages/login")
 })
 
     // Cadastro Pet
     app.get('/cadastro-pet', (req, res) => {
-        res.sendFile(__dirname + "/publico/paginas/cad-pet.html")
+        res.render("pages/cad-pet")
     })
 
     app.post('/cadastro-pet/add', (req, res) => {
@@ -78,7 +90,7 @@ app.get('/login', (req, res) => {
 
     // Cadastro usuário
     app.get('/cadastro-usuario', (req, res) => {
-        res.sendFile(__dirname + "/publico/paginas/cad-u.html")
+        res.render("pages/cad-u")
     })
     
     app.post('/cadastro-usuario/add', (req, res) => {
